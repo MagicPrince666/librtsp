2948c80add9a0dac2f90f3151343906b3f901e8d
MagicPrince666 <846863428@qq.com>
2022-11-17 01:14:24 +0800

    退出崩溃问题处理

--- example/example.cpp
+++ example/example.cpp
@@ -15,10 +15,65 @@
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <unistd.h>
+
 #define H264_FILENAME "../example/test.h264"
 #define SOCKET_ERROR (-1)
 #define INVALID_SOCKET (-1)
 #define BUF_SIZE 2048
+
+#define BACKTRACE_DEBUG 1
+
+#if BACKTRACE_DEBUG
+#include <signal.h>
+#include <execinfo.h>
+
+#define PRINT_SIZE_ 100
+
+static void _signal_handler(int signum)
+{
+    void *array[PRINT_SIZE_];
+    char **strings;
+
+    size_t size = backtrace(array, PRINT_SIZE_);
+    strings = backtrace_symbols(array, size);
+
+    if (strings == nullptr) {
+	   fprintf(stderr, "backtrace_symbols");
+	   exit(EXIT_FAILURE);
+    }
+
+    switch(signum) {
+        case SIGSEGV:
+        fprintf(stderr, "widebright received SIGSEGV! Stack trace:\n");
+        break;
+
+        case SIGPIPE:
+        fprintf(stderr, "widebright received SIGPIPE! Stack trace:\n");
+        break;
+
+        case SIGFPE:
+        fprintf(stderr, "widebright received SIGFPE! Stack trace:\n");
+        break;
+
+        case SIGABRT:
+        fprintf(stderr, "widebright received SIGABRT! Stack trace:\n");
+        break;
+
+        default:
+        break;
+    }
+
+    for (size_t i = 0; i < size; i++) {
+        fprintf(stderr, "%zu %s \n", i, strings[i]);
+    }
+
+    free(strings);
+    signal(signum, SIG_DFL); /* 还原默认的信号处理handler */
+
+    exit(1);
+}
+#endif
+
 typedef struct {
     unsigned char *data;
     int len;
@@ -209,6 +264,12 @@ void *rtsp_thread(void *args)
 
 int main(int argc, char *argv[])
 {
+#if BACKTRACE_DEBUG
+    signal(SIGPIPE, _signal_handler);  // SIGPIPE，管道破裂。
+    signal(SIGSEGV, _signal_handler);  // SIGSEGV，非法内存访问
+    signal(SIGFPE, _signal_handler);  // SIGFPE，数学相关的异常，如被0除，浮点溢出，等等
+    signal(SIGABRT, _signal_handler);  // SIGABRT，由调用abort函数产生，进程非正常退出
+#endif
     pthread_t rtsp_id;
     pthread_create(&rtsp_id, NULL, rtsp_thread, &ip);
     while (1) {
