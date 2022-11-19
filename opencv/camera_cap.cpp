9b36e6757a107cc213d50a9f22d69be3a173d07a
MagicPrince666 <846863428@qq.com>
2022-11-19 13:09:49 +0800

    更新接口

--- opencv/camera_cap.cpp
+++ opencv/camera_cap.cpp
@@ -9,26 +9,26 @@
 int main(int argc, char *argv[])
 {
     cv::VideoCapture capture(-1);
-    capture.set(CV_CAP_PROP_FRAME_WIDTH, 1920);
-    capture.set(CV_CAP_PROP_FRAME_HEIGHT, 1080);
-    capture.set(CV_CAP_PROP_FOURCC, CV_FOURCC('M', 'J', 'P', 'G'));
-    // capture.set(CV_CAP_PROP_FPS, 30);//帧数
+    capture.set(cv::CAP_PROP_FRAME_WIDTH, 1920);
+    capture.set(cv::CAP_PROP_FRAME_HEIGHT, 1080);
+    capture.set(cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'));
+    capture.set(cv::CAP_PROP_FPS, 30);//帧数
 
-    // capture.set(CV_CAP_PROP_BRIGHTNESS, 1);//亮度 50
-    // capture.set(CV_CAP_PROP_CONTRAST, 40);//对比度 50
-    // capture.set(CV_CAP_PROP_SATURATION, 50);//饱和度 50
-    // capture.set(CV_CAP_PROP_HUE, 50);//色调 0
-    // capture.set(CV_CAP_PROP_EXPOSURE, 50);//曝光 -12
+    // capture.set(cv::CAP_PROP_BRIGHTNESS, 1);//亮度 50
+    // capture.set(cv::CAP_PROP_CONTRAST, 40);//对比度 50
+    // capture.set(cv::CAP_PROP_SATURATION, 50);//饱和度 50
+    // capture.set(cv::CAP_PROP_HUE, 50);//色调 0
+    // capture.set(cv::CAP_PROP_EXPOSURE, 50);//曝光 -12
 
     //打印摄像头参数
-    spdlog::debug("width = {}", capture.get(CV_CAP_PROP_FRAME_WIDTH));
-    spdlog::debug("height = {}", capture.get(CV_CAP_PROP_FRAME_HEIGHT));
-    spdlog::debug("fbs = {}", capture.get(CV_CAP_PROP_FPS));
-    spdlog::debug("brightness = {}", capture.get(CV_CAP_PROP_BRIGHTNESS));
-    spdlog::debug("contrast = {}", capture.get(CV_CAP_PROP_CONTRAST));
-    spdlog::debug("saturation = {}", capture.get(CV_CAP_PROP_SATURATION));
-    spdlog::debug("hue = {}", capture.get(CV_CAP_PROP_HUE));
-    spdlog::debug("exposure = {}", capture.get(CV_CAP_PROP_EXPOSURE));
+    spdlog::debug("width = {}", capture.get(cv::CAP_PROP_FRAME_WIDTH));
+    spdlog::debug("height = {}", capture.get(cv::CAP_PROP_FRAME_HEIGHT));
+    spdlog::debug("fbs = {}", capture.get(cv::CAP_PROP_FPS));
+    spdlog::debug("brightness = {}", capture.get(cv::CAP_PROP_BRIGHTNESS));
+    spdlog::debug("contrast = {}", capture.get(cv::CAP_PROP_CONTRAST));
+    spdlog::debug("saturation = {}", capture.get(cv::CAP_PROP_SATURATION));
+    spdlog::debug("hue = {}", capture.get(cv::CAP_PROP_HUE));
+    spdlog::debug("exposure = {}", capture.get(cv::CAP_PROP_EXPOSURE));
     while (1) {
         cv::Mat frame;
         capture >> frame;
