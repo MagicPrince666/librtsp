b3c55f5cdd14647236caa2b98b6e2d894e656f77
MagicPrince666 <846863428@qq.com>
2024-08-21 07:48:41 +0000

    更新cmake

---
+++ v4l2demo/camera/mpi_enc_test.h
@@ -0,0 +1,14 @@
+#ifndef __MPI_ENC_TEST_H__
+#define __MPI_ENC_TEST_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int test(int argc, char **argv);
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif
